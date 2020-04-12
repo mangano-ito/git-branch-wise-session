23ccbe5adaecd659443a367f6b6eb25356b870e8
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2020-04-12 17:33:11 +0900

    add configuration model

---
+++ src/Model/Config.ts
@@ -0,0 +1,7 @@
+/**
+ * Extension configurations
+ */
+export default interface Config {
+    /** On branch switches, whether the extension should also switch sessions automatically. */
+    shouldAutoRestoreOnBranchSwitches: boolean;
+}
