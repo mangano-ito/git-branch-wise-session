b8a39a7907ba70f2bf5563a74f8599bdcbe1be3e
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2020-04-12 17:17:16 +0900

    add session repository interface

---
+++ src/Repository/Session/SessionRepository.ts
@@ -0,0 +1,37 @@
+import Session from '#/Model/Session';
+
+/** listener called on session updates */
+export type OnSessionUpdatedListener = () => any;
+
+/**
+ * Session Repository Interface
+ */
+export default interface SessionRepository {
+    /**
+     * get saved session by name
+     * @param name name of session to get
+     * @returns {Session | undefined} session found, otherwise undefined
+     */
+    get(name: string): Session | undefined;
+
+    /**
+     * removed specified saved session
+     * @param name name of session to remove
+     */
+    remove(name: string): any;
+
+    /**
+     * update given session
+     * @param session session to update
+     */
+    set(session: Session): any;
+
+    /** enumerated all saved sessions */
+    allSessions: Session[];
+
+    /**
+     * attach a listener called on updates
+     * @param listener listener to attach
+     */
+    setOnSessionUpdatedListener(listener: OnSessionUpdatedListener): any;
+}
