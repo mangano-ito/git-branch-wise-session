bc804a1874227cb1bafe85ef278f0212ee71a13d
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2020-04-12 17:50:42 +0900

    add configuration provider

---
+++ src/Provider/ConfigProvider.ts
@@ -0,0 +1,39 @@
+import * as vscode from 'vscode';
+
+import Config from '#/Model/Config';
+
+/** listener called when configurations get updated */
+type OnConfigDirtyListener = (config: Config) => any;
+
+export default class ConfigProvider {
+    private onConfigDirtyListener: OnConfigDirtyListener = () => {};
+    private config: vscode.WorkspaceConfiguration;
+
+    constructor(context: vscode.ExtensionContext) {
+        this.config = vscode.workspace.getConfiguration('git-branch-wise-session');
+        context.subscriptions.push(
+            vscode.workspace.onDidChangeConfiguration((e: vscode.ConfigurationChangeEvent) => {
+                this.onConfigDirtyListener(this.provide());
+            }),
+        );
+    }
+
+    /**
+     * provides the saved configuration
+     */
+    provide(): Config {
+        const shouldAutoRestoreOnBranchSwitches = this.config.get<boolean>('should-auto-restore-on-branch-switches') || true;
+
+        return {
+            shouldAutoRestoreOnBranchSwitches,
+        };
+    }
+
+    /**
+     * set a listener called when some of configurations get updated
+     * @param listener listener to attach
+     */
+    setOnConfigDirtyListener(listener: OnConfigDirtyListener) {
+        this.onConfigDirtyListener = listener;
+    }
+}
